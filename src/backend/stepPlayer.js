6387d5fff9209761522b4e5290106b777bffea86
Frederik Leidloff <kontakt@frederik-leidloff.de>
2019-12-10 21:33:38 +0100

    remove obsolete getter

--- src/backend/stepPlayer.js
+++ src/backend/stepPlayer.js
@@ -4,14 +4,12 @@ const stepPlayer = {
   sounds: [],
 
   play(step, song) {
-    this.getSounds().forEach(({ instrument, sound  }) => {
+    this.sounds.forEach(({ instrument, sound  }) => {
       song[instrument] &&
         song[instrument].includes(step) &&
         player.play(sound);
     });
   },
-
-  getSounds() { return  this.sounds },
 
   setSounds(sounds) {
     this.sounds = Object.keys(sounds).map(instrument => ({
